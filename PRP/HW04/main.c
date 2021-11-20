398f45f0d5c4757b82b31809d232e6f9848e6a6f
DanieIoZ <danielozcarl@gmail.com>
2021-11-20 16:49:58 +0100

    da

--- PRP/HW04/main.c
+++ PRP/HW04/main.c
@@ -1,14 +1,71 @@
 #include <stdio.h>
+#include <string.h>
 
 #define biggest_prime_number 1000000
 
-/*
-For this task it's enough to take 100000 primes, 
-but if the max. value wasn't 1 million, 
-it could be enough to take biggest_prime_number/4 size 
-for any possible number of primes.
-*/
 #define primes_count 100000
+
+
+void copy_to(char * src, char * result)
+{
+    int i = 0;
+    while (src[i] != '\0')
+    {
+        result[i] = src[i];
+        i++;
+    }
+    result[i] = '\0';
+}
+
+int divide(char * number, int divider, int copy)
+{
+    char division_result[100];
+    // Getting the smallest number, which can be divided by the divider
+    // - '0'    is needed, because digits in ASCII are not represented as 0..9 in integers
+    int num_size = 0;
+    while (number[num_size] != '\0')
+    {
+        num_size++;
+    }
+
+    int curr_digit = 0;
+    int mod = number[curr_digit++] - '0';
+    while (mod < divider && curr_digit < num_size)
+    {
+        mod = mod * 10 + (number[curr_digit++] - '0');
+
+    }
+    int division_modulo = 0;
+    if (mod < divider)
+    {
+        division_result[0] = '0';
+        division_result[1] = '\0';
+        division_modulo = mod;
+    }
+    else
+    {
+        //length of the result of the division
+        int length = 0;
+        //length of the original number
+
+        while (num_size >= curr_digit) 
+        {
+            division_result[length++] = (mod / divider) + '0';
+            
+
+            mod = (mod % divider) * 10 + number[curr_digit++] - '0';
+            
+            division_modulo = mod;
+            if (mod < 0)
+                division_modulo = mod + '0';
+        }
+        division_result[length] = '\0';
+    }
+    
+    if (copy)
+        copy_to(division_result, number);
+    return division_modulo;
+}
 
 int* get_sieve()
 {
@@ -41,35 +98,49 @@ int main()
     int *sieve = get_sieve();
     while (1)
     {
-        long long number = -1;
-        if (scanf("%lli", &number) == 0 || number < 0)
+        char number[100];
+        if (scanf("%s", number) == 0)
         {
             fprintf(stderr, "Error: Chybny vstup!\n");
             return 100;
         }
+        int i = 0;
+        while (number[i] != '\0')
+        {
+            if (number[i] < '0' || number[i] > '9')
+            {
+                fprintf(stderr, "Error: Chybny vstup!\n");
+                return 100;
+            }
+            i++;
+        }
 
-        if (number == 0)
+        if (strcmp(number, "0") == 0)
         {
             break;
         }
 
-        if (number == 1)
+        if (strcmp(number, "1") == 0)
         {
             printf("Prvociselny rozklad cisla 1 je:\n1\n");
         }
         else
         {
-            printf("Prvociselny rozklad cisla %lli je:\n", number);
+            printf("Prvociselny rozklad cisla %s je:\n", number);
             int divider = 0;
             while (1)
             {
-                if (number % sieve[divider] == 0)
+                char tmp_number[100];
+                copy_to(number, tmp_number);
+                int division_modulo = divide(tmp_number, sieve[divider], 1);
+                if (division_modulo == 0)
                 {
                     int divider_repeating = 0;
-                    while (number % sieve[divider] == 0)
+                    while (division_modulo == 0)
                     {
                         divider_repeating++;
-                        number /= sieve[divider];
+                        copy_to(tmp_number, number);
+                        division_modulo = divide(tmp_number, sieve[divider], 1);
                     }
                     
                     if (divider_repeating == 1)
@@ -80,8 +151,7 @@ int main()
                     {
                         printf("%d^%d", sieve[divider], divider_repeating);
                     }
-                    
-                    if (number != 1)
+                    if (division_modulo != 1)
                     {
                         printf(" x ");
                     }
