037d0821600878f0410875ccbd3bda7ad6380f45
DanieIoZ <danielozcarl@gmail.com>
2021-11-24 20:43:53 +0100

    d

--- PRP/HW06/main.c
+++ PRP/HW06/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_OPERATIONS 50
+#define MAX_OPERATIONS 100
 
 typedef struct matrix
 {
@@ -9,6 +9,37 @@ typedef struct matrix
     int size[2];
 } matrix;
 
+
+//These vars are global to free() them easier
+matrix ** mats;
+int last;
+
+void mt_free(matrix ** mat)
+{
+    if ((*mat)->data != NULL)
+    {
+        for (size_t i = 0; i < (*mat)->size[0]; i++)
+        {
+            if ((*mat)->data[i] != NULL)
+            {
+                free((*mat)->data[i]);
+            }
+        }
+
+        free((*mat)->data);
+    }
+    free(*mat);
+    *mat = NULL;
+}
+
+void mt_free_all()
+{
+    for (size_t i = 0; i < last + 1; i++)
+    {
+        mt_free(&mats[i]);
+    }
+    free(mats);
+}
 
 void mt_print(matrix * mat)
 {
@@ -24,34 +55,40 @@ void mt_print(matrix * mat)
     
 }
 
-matrix * mt_read()
+void mt_read(matrix ** mat)
 {
-    matrix * mat = (matrix *)malloc(sizeof(struct matrix));
-    if (scanf("%d %d", &mat->size[0], &mat->size[1]) < 2 )
+    *mat = (matrix *)malloc(sizeof(struct matrix));
+    if (scanf("%d %d", &(*mat)->size[0], &(*mat)->size[1]) < 2 )
     {
         fprintf(stderr, "Error: Chybny vstup!\n");
+        mt_free_all();
         exit(100);
     }
 
-    mat->data = malloc(mat->size[0] * sizeof(int*));
-    for (size_t i = 0; i < mat->size[0]; i++)
+    (*mat)->data = malloc((*mat)->size[0] * sizeof(int*));
+    int ERR_WRONG_INPUT = 0;
+    for (size_t i = 0; i < (*mat)->size[0]; i++)
     {
-        mat->data[i] = malloc(mat->size[1] * sizeof(int));
-        for (size_t j = 0; j < mat->size[1]; j++)
+        (*mat)->data[i] = malloc((*mat)->size[1] * sizeof(int));
+        for (size_t j = 0; j < (*mat)->size[1]; j++)
         {
-            if (scanf("%d", &mat->data[i][j]) < 1)
+            if (scanf("%d", &(*mat)->data[i][j]) < 1)
             {
-                fprintf(stderr, "Error: Chybny vstup!\n");
-                exit(100);
+                ERR_WRONG_INPUT = 1;
             }
         }
     }
-    return mat;
+    if (ERR_WRONG_INPUT)
+    {
+        fprintf(stderr, "Error: Chybny vstup!\n");
+        mt_free_all();
+        exit(100);
+    }
 }
 
 matrix * mt_create(int rows, int cols)
 {
-    matrix * mat = malloc(sizeof(struct matrix));
+    matrix * mat = (matrix *)malloc(sizeof(struct matrix));
     
     mat->size[0] = rows;
     mat->size[1] = cols;
@@ -62,17 +99,6 @@ matrix * mt_create(int rows, int cols)
         mat->data[i] = malloc(mat->size[1] * sizeof(int));
     }
     return mat;
-}
-
-void mt_free(matrix * mat)
-{
-    for (size_t i = 0; i < mat->size[0]; i++)
-    {
-        free(mat->data[i]);
-    }
-    free(mat->data);
-    free(mat);
-    mat = NULL;
 }
 
 int mt_mul_cell(matrix * mt_destination, matrix * mt_multiplier, int row, int col)
@@ -90,6 +116,7 @@ void mt_compsize(matrix * mt_1, matrix * mt_2)
     if (mt_1->size[0] != mt_2->size[0] || mt_1->size[1] != mt_2->size[1])
     {
         fprintf(stderr, "Error: Chybny vstup!\n");
+        mt_free_all();
         exit(100);
     }
 }
@@ -99,6 +126,7 @@ void  mt_comp_for_mul(matrix * mt_1, matrix * mt_2)
     if (mt_1->size[1] != mt_2->size[0])
     {
         fprintf(stderr, "Error: Chybny vstup!\n");
+        mt_free_all();
         exit(100);
     }
 }
@@ -128,29 +156,37 @@ void mt_minus(matrix * mt_destination, matrix * mt_minus)
         }
     }
 }
-
-void mt_mul(matrix * mt_destination, matrix * mt_multiplier)
+//I used matrix ** to free() it there
+void mt_mul(matrix ** mt_destination, matrix * mt_multiplier)
 {
-    matrix * result = mt_create(mt_destination->size[0], mt_multiplier->size[1]);
-    for (size_t i = 0; i < mt_destination->size[0]; i++)
+    mt_comp_for_mul(*mt_destination, mt_multiplier);
+    matrix *result = mt_create((*mt_destination)->size[0], mt_multiplier->size[1]);
+    for (size_t i = 0; i < (*mt_destination)->size[0]; i++)
     {
         for (size_t j = 0; j < mt_multiplier->size[1]; j++)
         {
-            result->data[i][j] = mt_mul_cell(mt_destination, mt_multiplier, i, j);
+            result->data[i][j] = mt_mul_cell((*mt_destination), mt_multiplier, i, j);
         }
     }
-    mt_free(mt_destination);
-    *mt_destination = *result;
+    //deleting old data
+    for (size_t i = 0; i < (*mt_destination)->size[0]; i++)
+    {
+        free((*mt_destination)->data[i]);
+    }
+    free((*mt_destination)->data);
+    free((*mt_destination));
+    *mt_destination = NULL;
+    //replacing old data with new
+    *mt_destination = result;
 }
 
 int main()
 {
-
-    matrix ** mats = malloc(sizeof(matrix *));
+    mats = malloc(sizeof(matrix *));
+    last = 0;
     char operations[MAX_OPERATIONS];
 
-    int last = 0;
-    mats[last] = mt_read();
+    mt_read(&mats[last]);
     int scanning = 1;
     while (scanning)
     {
@@ -163,25 +199,26 @@ int main()
             if (operations[last] != '-' && operations[last] != '+' && operations[last] != '*')
             {
                 fprintf(stderr, "Error: Chybny vstup!\n");
+                mt_free_all();
                 exit(100);
             }
             mats = realloc(mats, sizeof(matrix *) * (++last + 1));
-            mats[last] = mt_read();
+            mt_read(&mats[last]);
         }
     }
 
     int mt_mul_last = 0;
+    int mt_tmp = 0;
     while (mt_mul_last < last)
     {
         if (operations[mt_mul_last] == '*')
         {
-            int mt_tmp = mt_mul_last;
-            while (operations[mt_mul_last] == '*')
-            {
-                mt_mul(mats[mt_tmp], mats[++mt_mul_last]);
-            }
+            mt_mul(&mats[mt_tmp], mats[++mt_mul_last]);
         }
-        mt_mul_last++;
+        else
+        {
+            mt_tmp = ++mt_mul_last;
+        }
     }
 
     for (size_t i = 0; i < last; i++)
@@ -198,9 +235,5 @@ int main()
     printf("%d %d\n", mats[0]->size[0], mats[0]->size[1]);
     mt_print(mats[0]);
     
-    for (size_t i = 0; i <= last; i++)
-    {
-        mt_free(mats[i]);
-    }
-    free(mats);
+    mt_free_all();
 }
